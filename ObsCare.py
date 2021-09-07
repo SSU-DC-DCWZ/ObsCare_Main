4535701d5ad34ee8e48c0fc3e7c54738a16e3275
Park Sejin <49185035+pseeej@users.noreply.github.com>
2021-09-07 19:25:37 +0900

    considering if cameras connected

--- ObsCare.py
+++ ObsCare.py
@@ -4,59 +4,77 @@ from PyQt5 import QtGui
 from ui.play_ui import *
 from Detect.falldetect import model, ImageViewer
 
+import cv2
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     # ui.play_ui의 WindowClass 이용하여 창 객체 생성
     myWindow = WindowClass()
-
-    # thread 생성
-    # 카메라 번호를 이용하여 객체 생성
-    # thread와 카메라 객체 연결
-    thread1 = QtCore.QThread()
-    thread1.start()
-    vid1 = model(None, 0, myWindow.alert_browser)
-    vid1.moveToThread(thread1)
-
-    thread2 = QtCore.QThread()
-    thread2.start()
-    vid2 = model(None, 2, myWindow.alert_browser)
-    vid2.moveToThread(thread2)
-
-    thread3 = QtCore.QThread()
-    thread3.start()
-    vid3 = model(None, 4, myWindow.alert_browser)
-    vid3.moveToThread(thread3)
-
-    thread4 = QtCore.QThread()
-    thread4.start()
-    vid4 = model(None, 6, myWindow.alert_browser)
-    vid4.moveToThread(thread4)
-
+    
     # 영상 재생에 대한 객체 생성
     image_viewer1 = ImageViewer()
     image_viewer2 = ImageViewer()
     image_viewer3 = ImageViewer()
     image_viewer4 = ImageViewer()
-
+    
+    # 현재 접근 가능한 카메라 확인 
+    camNums = []
+    for i in range(10):
+        tmp = cv2.VideoCapture(i)
+        if tmp.isOpened():
+            camNums.append(i)
+        tmp.release()
+    
+    # 접근 가능한 카메라 대수
+    length = len(camNums)
+    
+    # 각 카메라별로
+    # thread 생성
+    # 카메라 번호를 이용하여 객체 생성
+    # thread와 카메라 객체 연결
     # 영상 재생을 위한 배경과 카메라 신호 연결
-    vid1.VideoSignal.connect(image_viewer1.setImage)
-    vid2.VideoSignal.connect(image_viewer2.setImage)
-    vid3.VideoSignal.connect(image_viewer3.setImage)
-    vid4.VideoSignal.connect(image_viewer4.setImage)
-
     # 영상 재생
-    start_button = QtWidgets.QPushButton()
-    start_button.clicked.connect(vid1.start)
-    start_button.click()
-    start_button1 = QtWidgets.QPushButton()
-    start_button1.clicked.connect(vid2.start)
-    start_button1.click()
-    start_button2 = QtWidgets.QPushButton()
-    start_button2.clicked.connect(vid3.start)
-    start_button2.click()
-    start_button3 = QtWidgets.QPushButton()
-    start_button3.clicked.connect(vid4.start)
-    start_button3.click()
+    if length >= 1:
+        thread1 = QtCore.QThread()
+        thread1.start()
+        vid1 = model(None, 0, myWindow.alert_browser)
+        vid1.moveToThread(thread1)
+        vid1.VideoSignal.connect(image_viewer1.setImage)
+        start_button = QtWidgets.QPushButton()
+        start_button.clicked.connect(vid1.start)
+        start_button.click()
+        
+    if length >= 2
+        thread2= QtCore.QThread()
+        thread2start()
+        vid2= model(None, 0, myWindow.alert_browser)
+        vid2moveToThread(thread2)
+        vid2VideoSignal.connect(image_viewer2.setImage)
+        start_button2 = QtWidgets.QPushButton()
+        start_button2.clicked.connect(vid2.start)
+        start_button2.click()
+        
+    if length >= 3:
+        thread3 = QtCore.QThread()
+        thread3.start()
+        vid3 = model(None, 0, myWindow.alert_browser)
+        vid3.moveToThread(thread3)
+        vid3.VideoSignal.connect(image_viewer3.setImage)
+        start_button3 = QtWidgets.QPushButton()
+        start_button3.clicked.connect(vid3.start)
+        start_button3.click()
+        
+    if length >= 4:
+        thread4 = QtCore.QThread()
+        thread4.start()
+        vid4 = model(None, 0, myWindow.alert_browser)
+        vid4.moveToThread(thread4)
+        vid4.VideoSignal.connect(image_viewer4.setImage)
+        start_button4 = QtWidgets.QPushButton()
+        start_button4.clicked.connect(vid4.start)
+        start_button4.click()
+
+
 
     # video_layout에 영상 행, 열로 추가
     myWindow.video_layout.addWidget(image_viewer1, 0, 0)
@@ -69,13 +87,18 @@ if __name__ == '__main__':
     # app 실행
     app.exec_()
     # app 종료 후 생성한 model 객체 스트리밍 정지
-    vid1.stop()
-    vid2.stop()
-    vid3.stop()
-    vid4.stop()
-    thread1.quit()
-    thread2.quit()
-    thread3.quit()
-    thread4.quit()
+    if length >= 1:
+        vid1.stop()
+        thread1.quit()
+    if length >= 2:
+        vid1.stop()
+        thread1.quit()
+    if length >= 3:
+        vid1.stop()
+        thread1.quit()
+    if length >= 4:
+        vid1.stop()
+        thread1.quit()
+        
     # 시스템 종료
     sys.exit()
